9aeecbe0228f3ef18af82e6d6479326db4c59345
mrsyaban <mrsyaban99@gmail.com>
2023-10-03 23:05:35 +0700

    delete unused file

--- src/app/init.php
+++ src/app/init.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 require_once __DIR__ . "/core/app.php";
 require_once __DIR__ . "/core/router.php";
@@ -7,8 +6,3 @@ require_once __DIR__ . "/database/database.php";
 
 require_once __DIR__ . "/controllers/not_found.php";
 require_once __DIR__ . "/controllers/dashboard/get_dashboard.php";
-=======
-require_once 'config/config.php';
-require_once 'core/Controller.php';
-require_once 'core/App.php';
->>>>>>> 66c4f8f (setup controller)

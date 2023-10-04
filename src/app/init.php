d348a36134e13c0a8c8cae32af21b1e2a7c2a604
mrsyaban <mrsyaban99@gmail.com>
2023-10-04 15:04:31 +0700

    add missing in merge

--- src/app/init.php
+++ src/app/init.php
@@ -6,5 +6,7 @@ require_once __DIR__ . "/database/database.php";
 require_once __DIR__ . "/controllers/not_found.php";
 require_once __DIR__ . "/controllers/dashboard/get_dashboard.php";
 require_once __DIR__ . "/controllers/home/get_home.php";
+require_once __DIR__ . "/controllers/login/get_login.php";
+require_once __DIR__ . "/controllers/signup/get_signup.php";
 
 require_once __DIR__ . "/../public/fonts/fonts.php";
